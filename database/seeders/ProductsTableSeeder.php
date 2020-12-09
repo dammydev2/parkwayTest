ab6f9c47792d5b3239884def80c95cc9b3ec1017
dammydev2 <damilola.yakubu@yahoo.com>
2020-12-09 06:40:33 +0100

    product seeder

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            'name' => 'MacBook Pro',
+            'slug' => 'macbook-pro',
+            'details' => '15 inch, 1TB HDD, 32GB RAM',
+            'price' => 2499.99,
+            'shipping_cost' => 29.99,
+            'description' => 'MackBook Pro',
+            'category_id' => 1,
+            'brand_id' => 1,
+            'image_path' => 'https://www.notebookcheck.net/typo3temp/_processed_/4/7/csm_mbp13-gray-select-201610_84b0ed291f.jpg'
+        ]);
+
+        Product::create([
+            'name' => 'Dell Vostro 3557',
+            'slug' => 'vostro-3557',
+            'details' => '15 inch, 1TB HDD, 8GB RAM',
+            'price' => 1499.99,
+            'shipping_cost' => 19.99,
+            'description' => 'Dell Vostro 3557',
+            'category_id' => 1,
+            'brand_id' => 2,
+            'image_path' => 'https://images-eu.ssl-images-amazon.com/images/I/71wsC-ZENhL._AC_UL160_.jpg'
+        ]);
+
+        Product::create([
+            'name' => 'iPhone 11 Pro',
+            'slug' => 'iphone-11-pro',
+            'details' => '6.1 inch, 64GB 4GB RAM',
+            'price' => 649.99,
+            'shipping_cost' => 14.99,
+            'description' => 'iPhone 11 Pro',
+            'category_id' => 2,
+            'brand_id' => 1,
+            'image_path' => 'https://brain-images-ssl.cdn.dixons.com/1/7/10197971/s_10197971.jpg'
+        ]);
+
+        Product::create([
+            'name' => 'Remax 610D Headset',
+            'slug' => 'remax-610d',
+            'details' => '6.1 inch, 64GB 4GB RAM',
+            'price' => 8.99,
+            'shipping_cost' => 1.89,
+            'description' => 'Remax 610D Headset',
+            'category_id' => 3,
+            'brand_id' => 3,
+            'image_path' => 'https://gd.image-gmkt.com/li/487/588/1171588487.g_180-w-st_g.jpg'
+        ]);
+
+        Product::create([
+            'name' => 'Samsung LED TV',
+            'slug' => 'samsung-led-24',
+            'details' => '24 inch, LED Display, Resolution 1366 x 768',
+            'price' => 41.99,
+            'shipping_cost' => 12.59,
+            'description' => 'Samsung LED TV',
+            'category_id' => 4,
+            'brand_id' => 4,
+            'image_path' => 'https://images.samsung.com/is/image/samsung/levant-fhd-t5300-ua43t5300auxtw-frontblack-thumb-229857918?$240_240_PNG$'
+        ]);
+
+        Product::create([
+            'name' => 'Samsung Digital Camera',
+            'slug' => 'samsung-mv800',
+            'details' => '16.1MP, 5x Optical Zoom',
+            'price' => 144.99,
+            'shipping_cost' => 13.39,
+            'description' => 'Samsung Digital Camera',
+            'category_id' => 5,
+            'brand_id' => 4,
+            'image_path' => 'https://d3re0f381bckq9.cloudfront.net/18615780_1530012716838_300x300.jpg'
+        ]);
+
+        Product::create([
+            'name' => 'Huawei GR 5 2017',
+            'slug' => 'gr5-2017',
+            'details' => '5.5 inch, 32GB 4GB RAM',
+            'price' => 148.99,
+            'shipping_cost' => 6.79,
+            'description' => 'Huawei GR 5 2017',
+            'category_id' => 2,
+            'brand_id' => 5,
+            'image_path' => 'https://fdn2.gsmarena.com/vv/bigpic/huawei-nova-8-se.jpg'
+        ]);
+    }
+}
